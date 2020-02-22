e4fb9dfca5491b9c70dede2319cef5a79fa134cb
Vector <hiuon.x@gmail.com>
2020-02-22 16:47:05 +0300

    feat: add fields of messages

--- src/bsu/rfe/java/group8/lab7/SHUDEYKO/var10/MainFrame.java
+++ src/bsu/rfe/java/group8/lab7/SHUDEYKO/var10/MainFrame.java
@@ -10,11 +10,38 @@ public class MainFrame extends JFrame {
     private static final int WIDTH = 800;
     private static final int HEIGHT = 600;
 
+    private final JTextArea textAreaIncoming;
+    private final JTextArea textAreaOutgoing;
+
+    private static final int INCOMING_AREA_DEFAULT_ROWS = 10;
+    private static final int OUTGOING_AREA_DEFAULT_ROWS = 5;
+
+    private static final int FROM_FIELD_DEFAULT_COLUMNS = 10;
+    private static final int TO_FIELD_DEFAULT_COLUMNS = 20;
+
+    private final JTextField textFieldFrom;
+    private final JTextField textFieldTo;
+
     public MainFrame(){
         super(FRAME_TITLE);
         setSize(WIDTH, HEIGHT);
         Toolkit kit = Toolkit.getDefaultToolkit();
         setLocation((kit.getScreenSize().width - WIDTH)/2, (kit.getScreenSize().height - HEIGHT)/2);
+
+        textAreaIncoming = new JTextArea(INCOMING_AREA_DEFAULT_ROWS, 0);
+        textAreaIncoming.setEditable(false);
+
+        final JScrollPane scrollPaneIncoming = new JScrollPane(textAreaIncoming);
+
+        final JLabel labelFrom = new JLabel("Подпись");
+        final JLabel labelTo = new JLabel("Получатель");
+
+        textFieldFrom = new JTextField(FROM_FIELD_DEFAULT_COLUMNS);
+        textFieldTo = new JTextField(TO_FIELD_DEFAULT_COLUMNS);
+
+        textAreaOutgoing = new JTextArea(OUTGOING_AREA_DEFAULT_ROWS, 0);
+        final JScrollPane scrollPaneOutgoing = new JScrollPane(textAreaOutgoing);
+
     }
 
 
