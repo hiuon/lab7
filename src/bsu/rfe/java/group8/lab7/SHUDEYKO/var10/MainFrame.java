203216e96fc5252c2143b4b1bef8e126740b816f
Vector <hiuon.x@gmail.com>
2020-02-22 16:28:57 +0300

    feat: set size&title of MainFrame

--- src/bsu/rfe/java/group8/lab7/SHUDEYKO/var10/MainFrame.java
+++ src/bsu/rfe/java/group8/lab7/SHUDEYKO/var10/MainFrame.java
@@ -1,8 +1,23 @@
 package bsu.rfe.java.group8.lab7.SHUDEYKO.var10;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
+
+    private static final String FRAME_TITLE = "Клиент мгновенных сообщений";
+
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+
+    public MainFrame(){
+        super(FRAME_TITLE);
+        setSize(WIDTH, HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        setLocation((kit.getScreenSize().width - WIDTH)/2, (kit.getScreenSize().height - HEIGHT)/2);
+    }
+
+
     public static void main(String args[]){
         SwingUtilities.invokeLater(new Runnable() {
             @Override
